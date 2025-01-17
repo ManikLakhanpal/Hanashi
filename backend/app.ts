46c47fe8a6fc37008ca7cbb77963d34897379415
Manik Lakhanpal <lakhanpalmanik@gmail.com>
2025-01-17 10:14:49 +0530

    json and urlencoded enabled for express

--- backend/app.ts
+++ backend/app.ts
@@ -3,6 +3,9 @@ import express from "npm:express";
 const app = express();
 const PORT = 5000;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (_req: express.Request, res: express.Response) => {
     res.json({message: "server is running!"});
 });
